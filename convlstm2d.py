b588b50b10e08922985fb596bae6ecfd7f32e0b8
JIMIT JAISWAL <jaishwaljimit@gmail.com>
2019-06-21 13:55:57 +0530

    add some files
    add Convlstm2d.py and Weather Forecast notebook file.

---
+++ convlstm2d.py
@@ -0,0 +1,58 @@
+import torch
+from torch import nn
+import torch.nn.functional as f
+from torch.autograd import Variable
+
+
+# Define some constants
+KERNEL_SIZE = 3
+PADDING = KERNEL_SIZE // 2
+
+
+class ConvLSTMCell(nn.Module):
+    """
+    Generate a convolutional LSTM cell
+    """
+
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.Gates = nn.Conv2d(input_size + hidden_size, 4 * hidden_size, KERNEL_SIZE, padding=PADDING)
+
+    def forward(self, input_, prev_state):
+
+        # get batch and spatial sizes
+        batch_size = input_.data.size()[0]
+        spatial_size = input_.data.size()[2:]
+
+        # generate empty prev_state, if None is provided
+        if prev_state is None:
+            state_size = [batch_size, self.hidden_size] + list(spatial_size)
+            prev_state = (
+                Variable(torch.zeros(state_size)),
+                Variable(torch.zeros(state_size))
+            )
+
+        prev_hidden, prev_cell = prev_state
+
+        # data size is [batch, channel, height, width]
+        stacked_inputs = torch.cat((input_, prev_hidden), 1)
+        gates = self.Gates(stacked_inputs)
+
+        # chunk across channel dimension
+        in_gate, remember_gate, out_gate, cell_gate = gates.chunk(4, 1)
+
+        # apply sigmoid non linearity
+        in_gate = f.sigmoid(in_gate)
+        remember_gate = f.sigmoid(remember_gate)
+        out_gate = f.sigmoid(out_gate)
+
+        # apply tanh non linearity
+        cell_gate = f.tanh(cell_gate)
+
+        # compute current cell and hidden state
+        cell = (remember_gate * prev_cell) + (in_gate * cell_gate)
+        hidden = out_gate * f.tanh(cell)
+
+        return hidden, cell
