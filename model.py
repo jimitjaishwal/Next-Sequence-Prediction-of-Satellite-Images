fece8e4645836d4c24b48aa42c7e832fc9ff7ad6
JIMIT JAISWAL <jaishwaljimit@gmail.com>
2019-07-11 10:33:08 +0530

    updated all files

--- model.py
+++ model.py
@@ -16,23 +16,19 @@ class Encoder(nn.Module):
         self.down3 = down(48, 64)#48,64 
         self.down4 = down(64, 80)#64, 80
         self.cell = ConvLSTMCell(80, 80)
-    
-    def set_encoder(self, x):
+        
+
+    def forward(self, x, states = None):
+        ''' Forward pass through the network, returns the output logits '''
         x = x.cuda()
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-        output, hidden = self.cell(x5, None)
-        return x1, x2, x3, x4, x5, output, [output, hidden]
-        
-
-    def forward(self, x):
-        ''' Forward pass through the network, returns the output logits '''
-        x1, x2, x3, x4, x5, output, [output, hidden] = self.set_encoder(x)
-        
-        return x1, x2, x3, x4, x5, output, [output, hidden]
+        output, hidden = self.cell(x5, states)
+        states = [F.elu(output), F.elu(hidden)]
+        return [x1, x2, x3, x4, x5], F.elu(output), states
 
 class Decoder(nn.Module):
     
@@ -46,21 +42,17 @@ class Decoder(nn.Module):
         self.up4 = up(48, 16)
         self.outc = outconv(16, 1)
         
-    def set_decoder(self, x1, x2, x3, x4, x5, encoder_output, states):
-        hidden_h, hidden_c = self.cell(encoder_output, states)
-        x = self.up0(hidden_c, x5)
-        x = self.up1(x, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        x = self.outc(x)
-        return x, [hidden_h, hidden_c]
 
-    def forward(self, x1, x2, x3, x4, x5, output, states):
+    def forward(self, layers, output, states):
         ''' Forward pass through the network, returns the output logits '''
-        
-        x, states = self.set_decoder(x1, x2, x3, x4, x5, output, states)
-            
+        output, hidden = self.cell(output, states)
+        x = self.up0(F.elu(output), layers[4])
+        x = self.up1(x, layers[3])
+        x = self.up2(x, layers[2])
+        x = self.up3(x, layers[1])
+        x = self.up4(x, layers[0])
+        x = self.outc(x)
+        states = [F.elu(output), F.elu(hidden)]        
         return x, states
 
 import random
@@ -80,9 +72,11 @@ class Seq2Seq(nn.Module):
         time = targets.shape[0]
         
         outputs = torch.FloatTensor(torch.zeros((time, batch_size, 1, 256, 256))).to(device)
+
+        states = None
         
         for t in range(time):
-            x1, x2, x3, x4, x5, output, states = self.encoder(features[t])
+            x1, x2, x3, x4, x5, output, states = self.encoder(features[t], states)
             
         decoder_hidden = states
         layers = [x1, x2, x3, x4, x5]
@@ -97,4 +91,38 @@ class Seq2Seq(nn.Module):
             input = (output_encoder if teacher_force else decoder_hidden[0])
             
         return outputs
+
+
+
+import torch.nn.functional as F
+
+from unet_parts import *
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(UNet, self).__init__()
+        self.inc = inconv(n_channels, 16) #16
+        self.down1 = down(16, 32)#16, 32
+        self.down2 = down(32, 48)#32, 48
+        self.down3 = down(48, 64)#48,64 
+        self.down4 = down(64, 80)#64, 80
+        self.up1 = up(144, 64) #160, 48
+        self.up2 = up(112, 48)
+        self.up3 = up(80, 32)
+        self.up4 = up(48, 16)
+        self.outc = outconv(16, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
+        # x = F.sigmoid(x)
+        return x
       
--- network_parts.py
+++ network_parts.py
@@ -1,5 +1,3 @@
-# sub-parts of the U-Net model
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,7 +8,7 @@ class single_conv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(single_conv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.Conv2d(in_ch, out_ch, kernel_size=(3, 3), stride=2, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ELU(inplace = True),
         )
@@ -34,7 +32,6 @@ class down(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(down, self).__init__()
         self.mpconv = nn.Sequential(
-            nn.MaxPool2d(2),
             single_conv(in_ch, out_ch)
         )
 
@@ -44,42 +41,26 @@ class down(nn.Module):
 
 
 class up(nn.Module):
-    def __init__(self, in_ch, out_ch, bilinear=True):
+    def __init__(self, in_ch, out_ch, stride):
         super(up, self).__init__()
 
         #  would be a nice idea if the upsampling could be learned too,
         #  but my machine do not have enough memory to handle all those weights
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        else:
-            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
-
-        self.conv = single_conv(in_ch, out_ch)
+        self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=stride)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
         
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
+        # # input is CHW
+        # diffY = x2.size()[2] - x1.size()[2]
+        # diffX = x2.size()[3] - x1.size()[3]
 
-        x1 = F.pad(x1, (diffX // 2, diffX - diffX//2,
-                        diffY // 2, diffY - diffY//2))
+        # x1 = F.pad(x1, (diffX // 2, diffX - diffX//2,
+        #                 diffY // 2, diffY - diffY//2))
         
         # for padding issues, see 
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
 
         x = torch.cat([x2, x1], dim=1)
-        x = self.conv(x)
-        return x
-
-
-class outconv(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(outconv, self).__init__()
-        self.conv = nn.Conv2d(in_ch, out_ch, 1)
-
-    def forward(self, x):
-        x = self.conv(x)
         return x
